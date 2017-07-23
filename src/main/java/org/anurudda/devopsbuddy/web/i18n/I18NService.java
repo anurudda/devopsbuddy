190b5e4aab0be671b7a87d7cf9206b9ee761f1de
Dilan Anuruddha <anurudda@gmail.com>
2017-07-23 09:04:12 +0530

    Logging implementation - slf4j

--- src/main/java/org/anurudda/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/org/anurudda/devopsbuddy/web/i18n/I18NService.java
@@ -1,5 +1,6 @@
 package org.anurudda.devopsbuddy.web.i18n;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -15,7 +16,11 @@ public class I18NService {
     @Autowired
     private MessageSource messageSource;
 
+    /** The application logger */
+    private static final    Logger LOG = org.slf4j.LoggerFactory.getLogger(I18NService.class);
+
     public String getMessage(String messageId){
+        LOG.info("Returning i18n message for message id {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId, locale);
         
